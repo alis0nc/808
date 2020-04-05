2bcea4ecfb2cb2035eaaf5ef345ad0491f12e583
Alison Chan <alisonc@alisonc.net>
2020-04-05 18:29:39 -0400

    stack instrument rows per measure

--- src/components/Sequence.tsx
+++ src/components/Sequence.tsx
@@ -5,20 +5,47 @@ import times from 'lodash/times';
 // TODO: Move this to a model class
 const instruments = ['Kick', 'Snare', 'Open Hat', 'Closed Hat'];
 
-const InstrumentRow = (props: {instrument : string}) => {
+interface MeasureProps {
+  instrument: string | undefined,
+  offset: number,
+}
+
+const Measure = (props: MeasureProps = {instrument: undefined, offset: 0}) => {
+  const {instrument, offset} = props;
+  return (
+    <>
+      {times(8, (i) => (
+        <Grid.Column>
+          <Checkbox aria-label={`${instrument} ${i+1+offset}`}/>
+        </Grid.Column>
+      ))}
+    </>
+  );
+};
+
+interface InstrumentRowProps {
+  instrument: string,
+}
+
+const InstrumentRow = (props: InstrumentRowProps) => {
   const {instrument} = props;
   return (
     <Grid.Row>
-      <Grid.Column width={4}>
+      <Grid.Column textAlign='right' width={4}>
       {instrument}
       </Grid.Column>
       <Grid.Column width={12}>
-        <Grid>
-          {times(16, (i) => (
-            <Grid.Column>
-              <Checkbox aria-label={`${instrument} ${i+1}`}/>
-            </Grid.Column>
-          ))}
+        <Grid stackable columns={2}>
+          <Grid.Column>
+            <Grid columns={8}>
+              <Measure instrument={instrument} offset={0} />
+            </Grid>
+          </Grid.Column>
+          <Grid.Column>
+            <Grid columns={8}>
+              <Measure instrument={instrument} offset={8} />
+            </Grid>
+          </Grid.Column>
         </Grid>
       </Grid.Column>
     </Grid.Row>
@@ -27,7 +54,7 @@ const InstrumentRow = (props: {instrument : string}) => {
 export default function Sequence() {
   return (
     <Container>
-      <Grid>
+      <Grid celled>
         {instruments.map((instrument) => <InstrumentRow instrument={instrument} />)}
       </Grid>
     </Container>
