963571757b6f31c27ce126c47c1a0ef388449366
Alison Chan <alisonc@alisonc.net>
2020-04-05 18:09:30 -0400

    break out instrument row

--- src/components/Sequence.tsx
+++ src/components/Sequence.tsx
@@ -1,30 +1,34 @@
 import React from 'react';
-import { Container, Grid } from 'semantic-ui-react';
+import { Checkbox, Container, Grid } from 'semantic-ui-react';
 import times from 'lodash/times';
 
 // TODO: Move this to a model class
 const instruments = ['Kick', 'Snare', 'Open Hat', 'Closed Hat'];
 
+const InstrumentRow = (props: {instrument : string}) => {
+  const {instrument} = props;
+  return (
+    <Grid.Row>
+      <Grid.Column width={4}>
+      {instrument}
+      </Grid.Column>
+      <Grid.Column width={12}>
+        <Grid>
+          {times(16, (i) => (
+            <Grid.Column>
+              <Checkbox aria-label={`${instrument} ${i+1}`}/>
+            </Grid.Column>
+          ))}
+        </Grid>
+      </Grid.Column>
+    </Grid.Row>
+  )};
+
 export default function Sequence() {
   return (
     <Container>
       <Grid>
-        {/* TODO: Move this to its own class */}
-        {instruments.map((instrument) => 
-          <Grid.Row>
-            <Grid.Column width={4}>
-            {instrument}
-            </Grid.Column>
-            <Grid.Column width={12}>
-              <Grid>
-                {/* TODO: Checkboxes or something of that ilk */}
-                {times(16, (i) => (
-                  <Grid.Column>{i+1}</Grid.Column>
-                ))}
-              </Grid>
-            </Grid.Column>
-          </Grid.Row>
-        )}
+        {instruments.map((instrument) => <InstrumentRow instrument={instrument} />)}
       </Grid>
     </Container>
   )
